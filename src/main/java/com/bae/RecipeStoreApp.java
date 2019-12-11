4f2fad9dc7a34dc60811c7f25d924e383f0f3211
alex-creating <alex-russo1996@hotmail.co.uk>
2019-12-11 14:18:45 +0000

    Initial empty commit

---
+++ src/main/java/com/bae/RecipeStoreApp.java
@@ -0,0 +1,13 @@
+package com.bae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeStoreApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeStoreApp.class, args);
+	}
+
+}
