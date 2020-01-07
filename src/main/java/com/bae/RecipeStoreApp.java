eb3485923735bb29a64e33a6970b486d761bc10e
alex-creating <alex-russo1996@hotmail.co.uk>
2020-01-07 18:57:14 +0000

    front added and tomcat added

--- src/main/java/com/bae/RecipeStoreApp.java
+++ src/main/java/com/bae/RecipeStoreApp.java
@@ -2,9 +2,10 @@ package com.bae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RecipeStoreApp {
+public class RecipeStoreApp extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeStoreApp.class, args);
