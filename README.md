8519848e634dc1bee0e2ce3bcdbf0b52ca574b46
alex-creating <alex-russo1996@hotmail.co.uk>
2020-01-18 19:14:06 +0000

    formatting of front end

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
-#The  Recipe Store
+# The  Recipe Store
 
 
 This was my personal solo project assigned by QA Consulting due on the 20th of January.
 
 
-##Index
+## Index
 
-*Requirements
-	*Solution
-*Architecture
-*Testing
-    *Surefire Report
-*Deployment
-*Front End Design
-    *Initial Wireframe Design
-    *Final Design
-*Future Possibilities
-    *Initial Design Changes
-*Authors
+* Requirements
+	* Solution
+* Architecture
+* Testing
+    * Surefire Report
+* Deployment
+* Front End Design
+    * Initial Wireframe Design
+    * Final Design
+* Future Possibilities
+    * Initial Design Changes
+* Authors
 
 
 
--- src/main/resources/static/Recipe.js
+++ src/main/resources/static/Recipe.js
@@ -4,20 +4,6 @@ $(document).ready(function() {
     });
 });
 
-// var textContainer, textareaSize, input;
-// var autoSize = function () {
-//   // also can use textContent or innerText
-//   textareaSize.innerHTML = input.value + '\n';
-// };
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   textContainer = document.querySelector('.textarea-container');
-//   textareaSize = textContainer.querySelector('.textarea-size');
-//   input = textContainer.querySelector('textarea');
-
-//   autoSize();
-//   input.addEventListener('input', autoSize);
-// });
 
 function turnToInteger(stringObject){
     return Number.parseInt(stringObject);
